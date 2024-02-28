0bd2c0624044a069b6d3df1a25e2756b363c7655
LanderDC <75493579+LanderDC@users.noreply.github.com>
2024-02-28 13:31:53 +0100

    add rarefaction

--- alpha_diversity.R
+++ alpha_diversity.R
@@ -1,16 +1,18 @@
 here::i_am("alpha_diversity.R")
+library(tidyverse)
+library(microbiome)
+library(vegan)
+library(ggpubr)
 
 # Load your workspace from a .RData file
 load("Sanon_16S_DADA2_data.RData")
 ##################################################################################################################
 ##### clean up your sequence
-library(microbiome)
+#ps.fam <- subset_taxa(Sanon, !is.na(Family) & !Family %in% c("", "uncharacterized"))
 
-ps.fam <- subset_taxa(Sanon, !is.na(Family) & !Family %in% c("", "uncharacterized"))
+#SANON<-ps.fam
 
-SANON<-ps.fam
-
-SANON
+SANON <- Sanon
 
 tax_table(SANON)[1,]
 
@@ -43,27 +45,80 @@ print(SANON)
 # now check taxa names are ASVids
 taxa_names(SANON)[1:3]
 
-# Check 
-taxa_names(SANON)[1:10]
-
 phyloseq::tax_table(SANON)[1:6]
 
 abou<-phyloseq::tax_table(SANON)
 abou
 
 #### Sort samples as you want they appear in your graph
-levels <- c("wa", "lar", "pu", "ad" ) 
-
 ## Alpha diversity measures per developmental stages
 
-plot_richness(SANON, x="Stage", measures=c("Simpson", "Shannon"), color = "Stage") +
-  geom_boxplot() +
-  theme_classic() +
- # levels(c("wa", "lar", "pu", "ad"))+
-  #stat_compare_means(comparisons = my_comparisons, label = "p.signif", 
-  #                   method = "wilcox.test", hide.ns = F, tip.length = 0)+
-  stat_pwc(method = "wilcox.test", p.adjust.method = "BH", label="p.adj.format", tip.length = 0)+
-  theme(strip.background = element_blank(), axis.text.x.bottom = element_text(angle = -90 ))
+#plot_richness(SANON, x="Stage", measures=c("Simpson", "Shannon"), color = "Stage") +
+#  geom_boxplot() +
+#  theme_classic() +
+#  ggpubr::stat_pwc(method = "wilcox.test", p.adjust.method = "BH", label="p.adj.format", tip.length = 0)+
+#  theme(strip.background = element_blank(), axis.text.x.bottom = element_text(angle = -90 ))
+
+
+alpha_rarefied <- function(ab_table, sequencing_depth) {
+  df <- ab_table %>%
+    t() %>% 
+    rrarefy(., sample=sequencing_depth) %>% # rrafefy samples from rows, not from columns!
+    as_tibble(rownames="sample") %>%
+    group_by(sample) %>%
+    pivot_longer(-sample) %>%
+    summarize(Observed = specnumber(value),
+              Shannon = diversity(value, index="shannon"),
+              Simpson = diversity(value, index="simpson")
+    ) %>%
+    as.data.frame() %>%
+    column_to_rownames("sample")
+  df
+}
+
+#' ## Alpha diversity
+df <- as_tibble(sample_data(SANON))
+df$LibrarySize <- sample_sums(SANON)
+df<- df[order(df$LibrarySize),]
+
+#threshold <- quantile(df$LibrarySize, 0.05)
+
+min_depth <- df %>% 
+  select(LibrarySize) %>% 
+  min()
+
+ggplot(data=df, aes(x=LibrarySize, y="samples")) + 
+  geom_jitter(height = 0.02)+
+  ggtitle("Library sizes")+
+  geom_vline(xintercept = min_depth, linetype="dashed", color="grey")+
+  labs(y="")+
+  #scale_x_log10()+
+  theme_bw()
+
+set.seed(1234)
+alpha_df_list <- purrr::map(1:10, ~alpha_rarefied(ab_table=as_tibble(otu_table(SANON)), sequencing_depth=min_depth))
+alpha_average_df <- Reduce(`+`, alpha_df_list) / length(alpha_df_list)
+alpha_average_df <- alpha_average_df %>% 
+  mutate(Observed=round(Observed))
+
+alpha_average_df %>% 
+  rownames_to_column("Sample") %>% 
+  select(-Observed) %>% 
+  left_join(samdf %>% rownames_to_column("Sample")) %>% 
+  pivot_longer(c(-Sample, -Stage, -Breeding, -Sites, -Urbanisation), names_to = "Metric", values_to = "Diversity") %>%
+  ggplot(aes(x=Stage, y=Diversity, color=Stage))+
+  geom_boxplot(outlier.shape = NA)+
+  geom_jitter(width=0.2)+
+  facet_wrap(~Metric, nrow=1, scales="free_y")+
+  theme_bw()+
+  theme(strip.text.x = element_text(size = 10),
+        axis.title.x = element_blank(),
+        legend.text.align = 0)+
+  scale_color_brewer(palette = "Set1")+
+  stat_pwc(method = "wilcox.test", p.adjust.method="BH",
+           label = "p.adj.format", hide.ns="p.adj", show.legend = F, tip.length = 0.01)
+alpha
+
 
 # Pick relative abundances (compositional) and sample metadata 
 
